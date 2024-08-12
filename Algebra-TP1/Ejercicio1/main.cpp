95373bf178009c5f664c7945c9869bcc3593dce2
ianco99 <71154684+ianco99@users.noreply.github.com>
2024-08-12 12:34:53 -0300

    Program now draws segments

--- Algebra-TP1/Ejercicio1/main.cpp
+++ Algebra-TP1/Ejercicio1/main.cpp
@@ -4,23 +4,58 @@
 
 using namespace std;
 
+struct Segment
+{
+	Vector2 p1;
+	Vector2 p2;
+};
+
 int main()
 {
-	vector<Vector2> playerPoints;
+	
+	vector<Vector2> playerPoints = vector<Vector2>();
+	Segment playerSegments[4] = {};
 
 	const int screenWidth = 800;
 	const int screenHeight = 450;
+	bool drawSegment = false;
+
+	int currentSegment = 0;
 
 	InitWindow(screenWidth, screenHeight, "Algebra - Ejercicio 1");
 
 	//userloop
 	while (!WindowShouldClose())
 	{
-		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-			playerPoints.push_back(GetMousePosition());
+		if (playerPoints.size() < 8)
+		{
+			if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+			{
+				playerPoints.push_back(GetMousePosition());
+
+				if (playerPoints.size() % 2 == 0 && playerPoints.size() > 0)
+				{
+					cout << playerPoints.size();
+					Segment segment;
+					int last = playerPoints.size() - 1;
+
+					segment.p1 = playerPoints[last - 1];
+					segment.p2 = playerPoints[last];
+					playerSegments[currentSegment] = segment;
+					currentSegment++;
+				}
+
+			}
+		}
 
 
 		BeginDrawing();
+
+		for (int i = 0; i < currentSegment; i++)
+		{
+			DrawLine(playerSegments[i].p1.x, playerSegments[i].p1.y, playerSegments[i].p2.x, playerSegments[i].p2.y, BLUE);
+		}
+
 		for (int i = 0; i < playerPoints.size(); i++)
 		{
 			DrawCircle(playerPoints[i].x, playerPoints[i].y, 2.0f, RED);
