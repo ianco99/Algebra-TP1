3a6cf01f2c6888fed8f42553e479dd1c07b25b42
ianco99 <71154684+ianco99@users.noreply.github.com>
2024-08-12 12:06:31 -0300

    Rendering user points

--- Algebra-TP1/Ejercicio1/main.cpp
+++ Algebra-TP1/Ejercicio1/main.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include "raylib.h"
+#include <vector>
 
 using namespace std;
 
 int main()
 {
+	vector<Vector2> playerPoints;
+
+	const int screenWidth = 800;
+	const int screenHeight = 450;
+
+	InitWindow(screenWidth, screenHeight, "Algebra - Ejercicio 1");
+
+	while (!WindowShouldClose())
+	{
+		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+			playerPoints.push_back(GetMousePosition());
+
+
+		BeginDrawing();
+		for (int i = 0; i < playerPoints.size(); i++)
+		{
+			DrawCircle(playerPoints[i].x, playerPoints[i].y, 2.0f, RED);
+		}
+		EndDrawing();
+	}
 
 	return 0;
 }
