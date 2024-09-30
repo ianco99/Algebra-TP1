9a77a8e26c1464c6c04a5df3037b8dc5c92f6d24
ianco99 <71154684+ianco99@users.noreply.github.com>
2024-09-29 23:15:30 -0300

    complex numbers detector

--- Algebra-TP1/Ejercicio1/main.cpp
+++ Algebra-TP1/Ejercicio1/main.cpp
@@ -20,7 +20,7 @@ void Create4SidedFigure(vector<Segment> sidesCreated);
 bool StartMatchesEnd(Segment segmentA, Segment segmentB);
 bool EndMatchesStart(Segment segmentA, Segment segmentB);
 void CalculateAreaAndPerimeter(Segment figure[]);
-float CalculateAreaOfTriangle(float segmentAdistance, float segmentBdistance, float segmentCdistance);
+double CalculateAreaOfTriangle(float segmentAdistance, float segmentBdistance, float segmentCdistance);
 
 int main()
 {
@@ -135,7 +135,7 @@ void CreateSides(vector<Vector2> collisionPoints)
 
 			Vector2 currentPoint = collisionPoints[i];
 			Vector2 nextPoint = collisionPoints[j];
-			Segment side{ currentPoint, nextPoint, Vector2Distance(currentPoint,nextPoint)};
+			Segment side{ currentPoint, nextPoint, Vector2Distance(currentPoint,nextPoint) };
 
 			//for (int k = 0; k < sidesCreated.size(); k++)
 			//{
@@ -251,28 +251,33 @@ bool StartMatchesEnd(Segment segmentA, Segment segmentB)
 
 void CalculateAreaAndPerimeter(Segment figure[4])
 {
-	float totalArea = 0;
+	double totalArea = 0;
 
 	cout << "PERIMETER: " << figure[0].distance + figure[1].distance + figure[2].distance + figure[3].distance << endl;
 
-	Segment diagonal = { figure[0].p1, figure[3].p1, Vector2Distance(figure[0].p1, figure[3].p1) };
+	Segment diagonal = { figure[0].p1, figure[2].p1, Vector2Distance(figure[0].p1, figure[2].p1) };
 
 	totalArea += CalculateAreaOfTriangle(figure[0].distance, figure[1].distance, diagonal.distance);
 	totalArea += CalculateAreaOfTriangle(figure[2].distance, figure[3].distance, diagonal.distance);
 
-	cout << "AREA: " << totalArea;
+	if (isnan(totalArea))
+		cout << "AREA IS A COMPLEX NUMBER" << endl;
+	else
+		cout << "AREA: " << totalArea;
 }
 
 //Teorema de Heron
-float CalculateAreaOfTriangle(float segmentAdistance, float segmentBdistance, float segmentCdistance)
+double CalculateAreaOfTriangle(float segmentAdistance, float segmentBdistance, float segmentCdistance)
 {
-	float semiperimeter = (segmentAdistance + segmentBdistance + segmentCdistance) / 2.0f;
+	double semiperimeter = (segmentAdistance + segmentBdistance + segmentCdistance) / 2.0f;
 
-	float area = 0;
+	double area = 0;
+
 
 	area = sqrt(semiperimeter * (semiperimeter - segmentAdistance)
 		* (semiperimeter - segmentBdistance)
 		* (semiperimeter - segmentCdistance));
+
 
 	return area;
 }
